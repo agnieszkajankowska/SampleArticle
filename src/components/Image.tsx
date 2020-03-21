9dfd286d210fa9350ccb946f3ac9498b9e0c03b0
Agnieszka Jankowska <ajankowska0404@gmail.com>
2020-03-21 14:50:29 +0100

    Delete parts of default create react app

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const Image = () => {
-    return <img alt="dogs" src={`${process.env.PUBLIC_URL}/assets/terriersPhoto.jpg`} />
+    return <img alt="dogs" src={`${process.env.PUBLIC_URL}/assets/staffy_terrier_pups_1200x624.jpg`} />
 }
